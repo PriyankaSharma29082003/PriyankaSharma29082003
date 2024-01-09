6689b30dbcf84f9d56eef174d11efaae1eb7a507
PriyankaSharma29082003 <155939915+PriyankaSharma29082003@users.noreply.github.com>
2024-01-09 11:39:22 +0530

    Created using Colaboratory

---
+++ pihu.py
@@ -0,0 +1,71 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyN9JWZXcxNnU+EnogYtAjmX",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/PriyankaSharma29082003/PriyankaSharma29082003/blob/priyanka/pihu.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "a=int(input(\"enter the value a\"))\n",
+        "b=int(input(\"enter the value b\"))\n",
+        "c=int(a+b),(a*b),(a-b),(a/b),(a%b  )\n",
+        "print(c)\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 70
+        },
+        "id": "G6EGI4eNrRJm",
+        "outputId": "1b5a9ee6-7030-46c5-868d-a741cc423775"
+      },
+      "execution_count": 20,
+      "outputs": [
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": [
+            "enter the value a10\n",
+            "enter the value b20\n"
+          ]
+        },
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "'(30, 200, -10, 0.5, 10)'"
+            ],
+            "application/vnd.google.colaboratory.intrinsic+json": {
+              "type": "string"
+            }
+          },
+          "metadata": {},
+          "execution_count": 20
+        }
+      ]
+    }
+  ]
+}
